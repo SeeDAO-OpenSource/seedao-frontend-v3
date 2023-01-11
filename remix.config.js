7dcf1a4ce5034a9f9664fb46a3c97f2ef59a213b
RueiCiWang <fatfingererr@gmail.com>
2023-01-11 19:26:45 +0800

    update

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,10 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   serverBuildTarget: "netlify",
+  server:
+    process.env.NETLIFY || process.env.NETLIFY_LOCAL
+      ? "./server.js"
+      : undefined,
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
